40669aa8ea6031166b5f327ad8b2ac399759abbf
szymonhrehorowicz <szymhre@gmail.com>
2024-05-14 10:36:55 +0200

    basic analysis

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    double wynik=0.0;
+    ifstream plik("feep.pgm"); // Otwarcie pliku do odczytu
+
+    if (!plik) {
+        cerr << "Nie mo�na otworzy� pliku." << endl;
+        return 1;
+    }
+
+    string magic_number;
+    int width, height, scale;
+
+    plik >> magic_number >> width >> height >> scale; // Odczytanie magicznej liczby, szeroko�ci, wysoko�ci i skali szaro�ci
+
+    if (magic_number != "P2") {
+        cerr << "To nie jest plik PGM w formacie P2." << endl;
+        return 1;
+    }
+
+    cout << "Szerokosc obrazu: " << width << endl;
+    cout << "Wysokosc obrazu: " << height << endl;
+    cout << "Skala szarosci: " << scale << endl;
+
+    // Tablica dwuwymiarowa do przechowywania warto�ci szaro�ci
+    vector<vector<int>> szarosci(height, vector<int>(width));
+
+    // Odczytanie i zapisanie warto�ci szaro�ci do tablicy
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            plik >> szarosci[i][j];
+        }
+    }
+
+    // Wy�wietlenie warto�ci szaro�ci
+    //cout << "Wartosci szarosci:" << endl;
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j)
+        {
+        //---------------------------------------------
+
+            if(szarosci[i][j]<180)
+                {
+                    if(szarosci[i][j]<160 ){wynik=wynik+1.0;}
+                    else {wynik=wynik+0.5;}
+
+                }
+
+        //-------------------------------------------
+            //cout << szarosci[i][j] << " ";
+        }
+        //cout << endl;
+    }
+
+    wynik=wynik/(height*width);
+    printf("\nUdzial perlitu ");
+    printf("%f",wynik);
+    cout<<"%C"<<endl;
+    plik.close(); // Zamkni�cie pliku
+
+    return 0;
+}
